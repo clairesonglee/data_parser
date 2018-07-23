85452e95d0621d605171b375d1270ed6bbfe7fb5
Brian Park <Brian@brians-mbp.dhcp.wustl.edu>
2018-07-23 17:45:46 -0500

    malloc

--- dp.cu
+++ dp.cu
@@ -12,18 +12,18 @@ using namespace std;
 
 #define NUM_STATES 3
 #define NUM_CHARS  256
-#define NUM_THREADS 512
+#define NUM_THREADS 352
 #define NUM_LINES 51000
-#define NUM_BLOCKS 400
+#define NUM_BLOCKS 20
 
 #define BUFFER_SIZE 250000000 //in byte
 //#define INPUT_FILE "./input/go_track_trackspoints.csv"
 //#define INPUT_FILE "./input/gtt_double.csv"
-#define INPUT_FILE "./input/sfpd_plots.csv"
+#define INPUT_FILE "./input/sfpd_plots_400.csv"
 //#define INPUT_FILE "./input/flight_1_rgb_1.csv.csv"
 //#define INPUT_FILE "./input/bacteria_4.csv"
-//#define INPUT_FILE "./input/nla_3.csv"
-//#define INPUT_FILE "./input/plot2_4.csv"
+//#define INPUT_FILE "./input/nla.csv"
+//#define INPUT_FILE "./input/plot2.csv"
 
 
 
@@ -67,13 +67,14 @@ struct SA_op {
 */
 
 __global__
-void remove_empty_elements (int** input, int* len_array, int total_lines, long* index, long* temp_base, 
-                            int* offset_array,  int* output, int* output_line_num, int taxi_application) {
+void remove_empty_elements (int** input, int* len_array, int** off_array, int total_lines, long* index, long* temp_base, 
+                            int* offset_array,  int* output, int* output_line_num, int* array_index, int taxi_application) {
 
     __shared__ int s_line_num;
     __shared__ int base;
 
     int len;
+    int* off;
     int line_num;
 
     //get the next line to compute
@@ -89,6 +90,8 @@ void remove_empty_elements (int** input, int* len_array, int total_lines, long* 
 
         //get the length of the line
         len = len_array[line_num];
+        off = off_array[line_num];
+
 
         //get the offset in order to save the output into the correct place
         if(threadIdx.x == 0)
@@ -98,35 +101,19 @@ void remove_empty_elements (int** input, int* len_array, int total_lines, long* 
         // for loop for the line that is longer than NUM_THREADS(number of threads)
         for(int loop = threadIdx.x; loop < len; loop += NUM_THREADS) {
 
-            //special flag for the second run of data_parser. (if taxi_app is on, then it inserts comma in front of every line)
-            if(!taxi_application) {
                 //if the current character is in the line, it copies the value into the output array
                 if(loop < len){
-                     output[base + loop] = (input[line_num])[loop];
+                   //  output[base + loop] = (input[line_num])[loop];
+                   output[base + loop] = off[loop];
                    //  printf("%d\n", input[line_num][loop]);
                 }
-            }
-            else {
-                //if the current character is in the line, it copies the value into the output array
-                //also save the line_num for the taxi application
-                if(loop < len - 1 ){
-                    output_line_num[base + loop + 1] = line_num;
-                    output[base + loop + 1] = (input[line_num])[loop] + 2;
-                }
-            }
+            
+
         }
         __syncthreads();
 
         if(threadIdx.x == 0) {
-            
-            //speical flag for taxi_app that puts 0 in front of all lines and saves all line_num for each coordinate
-            if(taxi_application){
-                output[base] = 0;
-                output_line_num[base] = line_num;
-            }
-            //free the input array(it is dynamically allocated in merge_scan function)
-            free(input[line_num]);
-            //grab the next line
+    
             s_line_num = atomicInc((unsigned int*) index, INT_MAX);
         }
          __syncthreads();
@@ -134,12 +121,18 @@ void remove_empty_elements (int** input, int* len_array, int total_lines, long* 
         line_num =  s_line_num;
     }
 
+    if(threadIdx.x + blockIdx.x == 0) {
+        for(int i = 0; i < array_index[0]; i++) {
+            free(input[i]);
+        }
+    }
+
 }
 
 
 __global__
 void merge_scan (char* line, int* len_array, int* offset_array, int** output_array, 
-                 long* index, int total_lines, int* num_commas_array, SA* d_SA_Table, int* total_num_commas, uint8_t* d_E, int taxi_application){
+                 long* index, int* array_index, int total_lines, int* num_commas_array, int** off_commas_array, SA* d_SA_Table, int* total_num_commas, uint8_t* d_E, int taxi_application){
 
     typedef cub::BlockScan<SA, NUM_THREADS> BlockScan_exclusive_scan; // change name
     typedef cub::BlockScan<int, NUM_THREADS> BlockScan_exclusive_sum; //
@@ -149,8 +142,13 @@ void merge_scan (char* line, int* len_array, int* offset_array, int** output_arr
     __shared__ SA prev_value;
     __shared__ int prev_sum;
     __shared__ int s_line_num;
-    __shared__ int s_temp_array_size;
-    __shared__ int* s_temp_ptr;
+    __shared__ int s_array_num;
+    __shared__ int s_mem_space;
+    __shared__ int* s_current_array;
+    __shared__ int stack;
+
+
+    //clock_t c_start = clock();
 
     SA temp_prev_val;
     int temp_prev_sum;
@@ -162,24 +160,41 @@ void merge_scan (char* line, int* len_array, int* offset_array, int** output_arr
     int* temp_output_array;
     int temp_array_size;
 
+    int array_num;
+    //int stack ;
+
     //grab the next/new line to compute
     if(threadIdx.x == 0) {
         s_line_num = atomicInc((unsigned int*) index, INT_MAX);
+        s_array_num = atomicInc((unsigned int*) array_index, INT_MAX);
     }
+
     __syncthreads();
     line_num = s_line_num;
+    array_num = s_array_num;
+
+    temp_array_size = total_lines * 20 / NUM_BLOCKS;
+    //temp_array_size = 100;
+
+    if(threadIdx.x == 0) {
+        temp_output_array = (int*)malloc(sizeof(int) * temp_array_size);
+        output_array[array_num] = temp_output_array;
+        s_current_array = output_array[array_num];
+        s_mem_space = temp_array_size;
+        stack = 0;
+    }
+    __syncthreads();
+
+
+    // long clock_temp = 0;
+    // long clock_scan = 0;
+    // long clock_end = 0;
+    // long clock_malloc = 0;
+    // long clock_copy = 0;
 
     //if the current line is in the input file 
     while(line_num < total_lines ) {
-
-        temp_array_size = 15;
-        //dynamic memory allocation
-        if(threadIdx.x == 0) {
-            temp_output_array = (int*)malloc(sizeof(int) * temp_array_size);
-            output_array[line_num] = temp_output_array;
-        }
-
-
+        // clock_t c1 = clock();
         len = len_array[line_num];
         offset = offset_array[line_num];
 
@@ -192,8 +207,14 @@ void merge_scan (char* line, int* len_array, int* offset_array, int** output_arr
         int loop;
         __syncthreads();
 
+        // clock_t c2 = clock();
+        // clock_temp += (c2 - c1);
+        
+
         //If the string is longer than NUM_THREADS
         for(int ph = 0; ph < len; ph += NUM_THREADS) {
+
+           // clock_t c3 = clock();
 
             loop = threadIdx.x + ph;
             char c = 0;
@@ -217,44 +238,28 @@ void merge_scan (char* line, int* len_array, int* offset_array, int** output_arr
 
             //(if the array is full, then it doubles the size fo the array )
 
-            if(prev_sum + temp_prev_sum > temp_array_size){
-                int new_sum = prev_sum + temp_prev_sum;
-                int* temp_ptr;
-                //make a new array with double size
-                if(threadIdx.x == 0) {
-                    while(new_sum > temp_array_size) {
-                        temp_array_size = temp_array_size * 2;
-                    }
-                    s_temp_array_size = temp_array_size;
-                    s_temp_ptr = (int*)malloc(sizeof(int) * temp_array_size);
-                }
-                //all threads have the same ptr and the array size
-                __syncthreads();
-                temp_array_size = s_temp_array_size;
-                temp_ptr = s_temp_ptr;
-                //copy the data 
-                for(int j = 0; j < (int)ceilf((float) (prev_sum) / NUM_THREADS); j++) {
-                    int idx = threadIdx.x + j * NUM_THREADS;
-                    if(idx < prev_sum) {
-                        temp_ptr[idx] = output_array[line_num][idx];
-                    }
-                }
-                __syncthreads();
-
-                //free the old array
-                if(threadIdx.x == 0) {
-                    free(output_array[line_num]);
-                    output_array[line_num] = temp_ptr;
-                }   
+            if(prev_sum + temp_prev_sum > s_mem_space){
+                printf("new array\n");
+                 if(threadIdx.x == 0){        
+                     s_array_num = atomicInc((unsigned int*) array_index, INT_MAX);
+                     temp_output_array = (int*)malloc(sizeof(int) * temp_array_size);
+                     output_array[s_array_num] = temp_output_array;
+                     s_current_array = output_array[s_array_num];
+                     s_mem_space = temp_array_size;
+                     stack = 0;
+                  }
+                  __syncthreads();
+                  array_num = s_array_num;
             }
-
-
 
             __syncthreads();
 
             //save the data (comma_index)
             if(start == 1 && loop < len) {
-                (output_array[line_num])[end + prev_sum] = loop;
+                (output_array[array_num])[end + prev_sum + stack] = loop;
+                // if(array_num == 0) {
+                //     printf("end %d, stack: %d loop: %d\n", end + prev_sum, stack, loop);
+                // }
             }
 
             //save the end values for the next iteration
@@ -264,18 +269,24 @@ void merge_scan (char* line, int* len_array, int* offset_array, int** output_arr
 
                 prev_sum += temp_prev_sum;
             }
-
             __syncthreads();
 
+            // clock_t c4 = clock();
+            // clock_scan += c4 - c3;   
                     
         }
+      // clock_t c5 = clock();
 
         //if the last thread saves the number of commas in the line
         if(loop == len - 1) {
-            //for the taxi app, it stores one more space (in front of every line) - this will be filled in remove_empty_elements kernel function
-            if(taxi_application)
-                prev_sum++;
+            s_mem_space -= prev_sum;
             num_commas_array[line_num] = prev_sum;
+            off_commas_array[line_num] = s_current_array;
+            if(prev_sum > 0){
+                stack += prev_sum;
+            }
+
+            s_current_array = (int*) (s_current_array + prev_sum);
             //atomic operation to track total number of commas in the input file to properly allocated the space for the output array
             int temp = atomicAdd(total_num_commas, prev_sum);
         }
@@ -285,8 +296,26 @@ void merge_scan (char* line, int* len_array, int* offset_array, int** output_arr
             s_line_num = atomicInc((unsigned int*) index, INT_MAX);
          __syncthreads();
         line_num =  s_line_num;
+        // clock_t c6 = clock();
+        //     clock_end += c6 - c5;   
     }
+    // clock_t c_end = clock();
+    // if(threadIdx.x + blockIdx.x == 0){
+    //     printf("merge_scan tiem: %d\n", c_end - c_start);
+    //     // printf("setup time: %d\n", clock_temp);
+    //     // printf("malloc time: %d\n", clock_malloc);
+    //     printf("copy time: %d\n", clock_copy);
 
+    //     printf("scan time: %d\n", clock_scan);
+    //     printf("setup time2: %d\n", clock_end);
+    // }
+
+    __syncthreads();
+    // if(threadIdx.x + blockIdx.x == 0) {
+    //     for(int i = 0; i < temp_array_size; i++){
+    //         printf("%d ",output_array[0][i]);
+    //     }
+    // }
 }
 
 /*
@@ -502,6 +531,9 @@ int main() {
         int* d_num_commas;
         cudaMalloc((int**) &d_num_commas, line_count * sizeof(int));
 
+        int** d_off_commas;
+        cudaMalloc((int**) &d_off_commas, line_count * sizeof(int*));
+
 
         int* d_comma_offset_array;
         cudaMalloc((int**)&d_comma_offset_array, (line_count + 1) * sizeof(int));
@@ -515,12 +547,17 @@ int main() {
         int* d_total_num_commas;
         cudaMalloc((int**) &d_total_num_commas, sizeof(int));
 
+        int* d_array_index;
+        cudaMalloc((int**) &d_array_index, sizeof(int));
+
         cudaDeviceSynchronize();
         auto tt2 = Clock::now();
 
-        cout <<"Device M A:" <<std::chrono::duration_cast<std::chrono::microseconds>(tt2 - tt1).count() << " microseconds" << endl;
+       // cout <<"Device M A:" <<std::chrono::duration_cast<std::chrono::microseconds>(tt2 - tt1).count() << " microseconds" << endl;
 
         long temp = 0;
+        
+
         
         auto t1 = Clock::now();
 
@@ -531,31 +568,41 @@ int main() {
         cudaMemcpy(d_stack, &temp, sizeof(long), cudaMemcpyHostToDevice);
         cudaMemcpy(d_temp_base, &temp, sizeof(long), cudaMemcpyHostToDevice);
         cudaMemcpy(d_total_num_commas, &temp, sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_array_index, &temp, sizeof(int), cudaMemcpyHostToDevice);
 
         cudaDeviceSynchronize();
         auto t2 = Clock::now();
 
-
-        cout <<"Host to Device:" <<std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " microseconds" << endl;
+      //  cout <<"Host to Device:" <<std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " microseconds" << endl;
 
         // grid and block dimensions to launch kernel function
         dim3 dimGrid(NUM_BLOCKS,1,1);
         dim3 dimBlock(NUM_THREADS,1,1);
 
+        int nBlocksPerSM_suggested;
+        cudaOccupancyMaxActiveBlocksPerMultiprocessor(&nBlocksPerSM_suggested, merge_scan, NUM_THREADS, 4432);
+       // printf("nblock: %d\n", nBlocksPerSM_suggested);
 
+        //dim3 dimGrid_Merge(nBlocksPerSM_suggested * 100, 1, 1);
+        
         //t3 ~ t4 for merge_can and output sort
+        size_t limit = 0;
+        cudaDeviceGetLimit(&limit, cudaLimitMallocHeapSize);
+        printf("cudaLimitMallocHeapSize: %u\n", (unsigned)limit);
+        cudaDeviceSetLimit(cudaLimitMallocHeapSize, limit * 100);
+
         auto t3 = Clock::now();
 
+//         cudaDeviceSynchronize();
+//         cudaError_t e = cudaGetLastError();
+//         if(e!=cudaSuccess) {                                              
+//             printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           
+//         } 
         // function call to locate commas in input line
-        merge_scan<<<dimGrid, dimBlock>>>(d_buffer, d_len_array, d_offset_array, d_output_array, d_stack, line_count, d_num_commas, d_SA_Table, d_total_num_commas, d_E, 0);
+        merge_scan<<<dimGrid, dimBlock>>>(d_buffer, d_len_array, d_offset_array, d_output_array, d_stack, d_array_index, line_count, d_num_commas, d_off_commas, d_SA_Table, d_total_num_commas, d_E, 0);
         
 
 
-        //    cudaDeviceSynchronize();
-        // cudaError_t e = cudaGetLastError();
-        // if(e!=cudaSuccess) {                                              
-        //     printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           
-        // } 
         //offset array has the correct offset values
         output_sort<<<1, NUM_THREADS>>> (d_num_commas, line_count, d_comma_offset_array);
 
@@ -580,7 +627,7 @@ int main() {
         auto t5 = Clock::now();
 
         // launches kernel function to clear unnecessary information
-        remove_empty_elements<<<dimGrid, dimBlock>>> (d_output_array, d_num_commas, line_count, d_stack, d_temp_base, d_comma_offset_array, d_final_array, d_final_array /* temp array */, 0);
+        remove_empty_elements<<<dimGrid, dimBlock>>> (d_output_array, d_num_commas, d_off_commas, line_count, d_stack, d_temp_base, d_comma_offset_array, d_final_array, d_final_array /* temp array */, d_array_index, 0);
         
 
         cudaDeviceSynchronize();
@@ -599,16 +646,16 @@ int main() {
 
         cudaDeviceSynchronize();
         auto t8 = Clock::now();
-        cout << "Device to Host:" << std::chrono::duration_cast<std::chrono::microseconds>(t8 - t7).count() << " microseconds" << endl;
+       cout << "Device to Host:" << std::chrono::duration_cast<std::chrono::microseconds>(t8 - t7).count() << " microseconds" << endl;
 
-        for(int i = 0; i < line_count; i++){
-            int len = num_commas[i];
-            int off = comma_offset_array[i];
-            for(int j = 0; j < len; j++){
-                printf("%d ", final_array[off + j]);
-            }
-            printf("\n");
-        }
+        // for(int i = 0; i < line_count; i++){
+        //     int len = num_commas[i];
+        //     int off = comma_offset_array[i];
+        //     for(int j = 0; j < len; j++){
+        //         printf("%d ", final_array[off + j]);
+        //     }
+        //     printf("\n");
+        // }
 
      
         // device memory
