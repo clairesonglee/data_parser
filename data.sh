4adbb97799c8040126cfc5333f77d1b794a399c3
clairesonglee <clairesonglee@gmail.com>
2018-07-27 20:25:57 -0500

    changed script to support inputfiles

--- data.sh
+++ data.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 """
 # change params 
-sed -i 's/^#define INPUT_FILE .*$/#define INPUT_FILE '"testing.csv"'/' test.cu
 
-sed -i '' 's/^#define NUM_STATES .*$/#define NUM_STATES '$1'/' test.cpp
+sed -i '' 's+^#define INPUT_FILE .*$+#define INPUT_FILE '$1'+' test.cpp
 
-sed -i '' 's/^#define NUM_THREADS .*$/#define NUM_THREADS '$2'/' test.cpp
+sed -i '' 's/^#define NUM_STATES .*$/#define NUM_STATES '$2'/' test.cpp
+
+sed -i '' 's/^#define NUM_THREADS .*$/#define NUM_THREADS '$3'/' test.cpp
+
+
+#sed -i 's/^#define INPUT_FILE .*$/#define INPUT_FILE '"testing.csv"'/' test.cpp
 
 #compile and run exe 
 g++ -o test test.cpp && ./test
 
-# print output.txt contents 
-cat output.txt
-echo
 
 
 """
 
 # change params 
-#sed -i 's/^#define INPUT_FILE .*$/#define INPUT_FILE '"nla.csv"'/' test.cu
+sed -i 's+^#define INPUT_FILE .*$+#define INPUT_FILE '$1'+' test.cpp
 
 sed -i 's/^#define NUM_STATES .*$/#define NUM_STATES '$1'/' dp_auto.cu
 
@@ -30,3 +31,5 @@ nvcc -arch=compute_52 -code=sm_52 -o dp_auto dp_auto.cu -I/export/project/hondiu
 
 # print output.txt contents 
 cat output.txt >> out.txt 
+
+#"""
--- input.py
+++ input.py
@@ -4,23 +4,19 @@ import subprocess
 import shlex
 from subprocess import Popen
 
-# INPUT_FILES = ["./nla.csv"]
+INPUT_FILES = ["./test.csv"]
 NUM_STATES = [1,2,3,4,5,6,7,8,9,10]
 NUM_THREADS = [32,64,128,256]
 
-for j in range(len(NUM_STATES)):
-	for k in range(len(NUM_THREADS)):
-		var1 = str(NUM_STATES[j])
-		var2 = str(NUM_THREADS[k])
-		os.system("./data.sh %s %s" % (var1, var2))
+def add_quotes(s):
+	s = '"{}"'.format(s)
+	return "'%s'" % s
 
-# for i in INPUT_FILES:
-	# for j in NUM_STATES:
-	# 	for k in NUM_THREADS:
-		# var1 = INPUT_FILES[i]
-		# var2 = str(NUM_STATES[j])
-		# var3 = str(NUM_THREADS[k])
-		# os.system("./data.sh %s %s %s" % (var1, var2))
+for i in range(len(INPUT_FILES)):
+	for j in range(len(INPUT_FILES)):
+		for k in range(len(INPUT_FILES)):
+			var1 = str(add_quotes(INPUT_FILES[i]))
+			var2 = str(NUM_STATES[j])
+			var3 = str(NUM_THREADS[k])
 
-# Process=Popen(['./data.sh %s %s' % (str(NUM_STATES[j]),str(NUM_THREADS[k])], shell=True)
-# subprocess.call(shlex.split('./data.sh INPUT_FILES[i] NUM_STATES[j] NUM_THREADS[k]'))
+			os.system("./data.sh %s %s %s" % (var1, var2, var3))
